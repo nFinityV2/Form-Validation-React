ece11d9ff2b0573a55fa28d32d8f6ba4aa7a83ba
nFinityV2 <99304246+nFinityV2@users.noreply.github.com>
2023-06-30 20:55:02 +1000

    Additional Styles update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
+
 import 'react-toastify/dist/ReactToastify.css';
 import '../styles/form.css';
 
@@ -9,7 +10,7 @@ const RandomForm = () => {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+ 
   function validationForm(e) {
     e.preventDefault();
     const passwordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#%^&*?])[a-zA-Z0-9!@#$%^&*]{8,}$/;
@@ -57,7 +58,7 @@ const RandomForm = () => {
       });
     }
   }
-
+  
   return (
     <div className="form-container">
       <form method="submit" onSubmit={validationForm}>
---
+++ src/components/ThankYou.jsx
@@ -0,0 +1,12 @@
+import '../styles/thankyou.css'
+
+const ThankYou = () => {
+    return (
+        <section className="thankyou"> 
+            <h1>Thanks for signing up!</h1>
+            <h2>New newsletters will be sent every week!</h2>
+        </section>
+    )
+}
+
+export default ThankYou;
